2992b35dce27d8d1350d97b84731dca882e2f4a3
rayhan gustiar <rayhangustiar@gmail.com>
2024-03-04 14:28:53 +0700

    add control connection to redux

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
+import { moveDown, moveLeft, moveRight, rotate } from '../actions'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Controls = (props) => {
+    const dispatch = useDispatch()
+    const isRunning = useSelector((state) => state.isRunning)
     return (
         <div className="controls">
             {/* left */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(moveLeft())
             }}>Kiri</button>
 
             {/* right */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(moveRight())
             }}>Kanan</button>
 
             {/* rotate */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(rotate())
             }}>Putar</button>
 
             {/* down */}
             <button className="control-button" onClick={(e) => {
-
+                dispatch(moveDown())
             }}>Bawah</button>
 
         </div>
