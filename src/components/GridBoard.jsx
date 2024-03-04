23c295ae0e7df798092dc8b0fc17dd9d2665f220
rayhan gustiar <rayhangustiar@gmail.com>
2024-03-04 14:24:33 +0700

    add connection to gridboard

--- src/components/GridBoard.jsx
+++ src/components/GridBoard.jsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import GridSquare from './GridSquare'
+import { useSelector } from 'react-redux'
+import { shapes } from '../utils'
 
 //generate board tetrisnya (10 * 18 kotak)
 const GridBoard = (props) => {
-    const grid = []
 
-    //row itu yang vertikal
-    for (let row = 0; row < 18; row++) {
-        grid.push([])
+    const game = useSelector((state) => state.game)
+    const { grid, shape, rotation, x, y, isRunning, speed } = game
+    const block = shapes[shape][rotation]
+    const blockColor = shape
+    // map rows
+    const gridSquares = grid.map((rowArray, row) => {
+        // map columns
+        return rowArray.map((square, col) => {
+            // Find the block x and y on the shape grid
+            // By subtracting the x and y from the col and the row we get the position of the upper left corner of the block array as if it was superimposed over the main grid
+            const blockX = col - x
+            const blockY = row - y
+            let color = square
+            // Map current falling block to grid.
+            // For any squares that fall on the grid we need to look at the block array and see if there is a 1 in this case we use the block color.
+            if (blockX >= 0 && blockX < block.length && blockY >= 0 && blockY < block.length) {
+                color = block[blockY][blockX] === 0 ? color : blockColor
+            }
+            // Generate a unique key for every block
+            const k = row * grid[0].length + col;
+            // Generate a grid square
+            return <GridSquare
+                key={k}
+                color={color} />
+        })
+    })
 
-        //col yang horizontal
-        for (let col = 0; col < 10; col++) {
-            grid[row].push(<GridSquare key={`${col}${row}`} color="1"></GridSquare>)
-        }
-
-    }
     return (
         <div className='grid-board'>
-            {grid}
+            {gridSquares}
         </div>
     )
 }
