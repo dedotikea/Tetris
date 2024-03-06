ac8ce66dd5d699ea1175f13bb8f547da62d83bea
rayhan gustiar <rayhangustiar@gmail.com>
2024-03-06 14:20:08 +0700

    test fixing deploy error firefox

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/USK-Tetris/'
+  base: 'https://dedotikea.github.io/USK-Tetris/'
 })
