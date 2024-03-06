8f647a5043c3ca56ece6577e887e6a288504e536
rayhan gustiar <rayhangustiar@gmail.com>
2024-03-06 15:37:52 +0700

    add gameover

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -5,27 +5,44 @@ import { useDispatch, useSelector } from 'react-redux'
 const Controls = (props) => {
     const dispatch = useDispatch()
     const isRunning = useSelector((state) => state.isRunning)
+    const gameOver = useSelector((state) => state.game.gameOver)
     return (
         <div className="controls">
             {/* left */}
-            <button className="control-button" onClick={(e) => {
-                dispatch(moveLeft())
-            }}>Kiri</button>
+            <button
+                disabled={!isRunning || gameOver}
+                className="control-button"
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
+                    dispatch(moveLeft())
+                }}>Kiri</button>
 
             {/* right */}
-            <button className="control-button" onClick={(e) => {
-                dispatch(moveRight())
-            }}>Kanan</button>
+            <button
+                disabled={!isRunning || gameOver}
+                className="control-button"
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
+                    dispatch(moveRight())
+                }}>Kanan</button>
 
             {/* rotate */}
-            <button className="control-button" onClick={(e) => {
-                dispatch(rotate())
-            }}>Putar</button>
+            <button
+                disabled={!isRunning || gameOver}
+                className="control-button"
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
+                    dispatch(rotate())
+                }}>Putar</button>
 
             {/* down */}
-            <button className="control-button" onClick={(e) => {
-                dispatch(moveDown())
-            }}>Bawah</button>
+            <button
+                disabled={!isRunning || gameOver}
+                className="control-button"
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
+                    dispatch(moveDown())
+                }}>Bawah</button>
 
         </div>
     )
--- src/reducers/game-reducer.jsx
+++ src/reducers/game-reducer.jsx
@@ -40,7 +40,17 @@ const gameReducer = (state = defaultState(), action) => {
                 return { ...state, y: maybeY }
             }
             // If not place the block
-            const newGrid = addBlockToGrid(shape, grid, x, y, rotation)
+            const obj = addBlockToGrid(shape, grid, x, y, rotation)
+            const newGrid = obj.grid
+            const gameOver = obj.gameOver
+
+            if (gameOver) {
+                // Game Over
+                const newState = { ...state }
+                newState.shape = 0
+                newState.grid = newGrid
+                return { ...state, gameOver: true }
+            }
             // reset some things to start a new shape/block
             const newState = defaultState()
             newState.grid = newGrid
